39645011d8cf878986011615329ed374cc221cc9
Mikuláš Poul <miki@laptop-miki.(none)>
2013-10-08 09:26:11 +0200

    Základní popis gramatiky našeho jazyka

--- gramatika.py
+++ gramatika.py
@@ -1,1 +1,11 @@
 #tady bude gramatika
+"""
+IDENTIFIKATOR ::= a..z|A..Z{0..0|a..z|A..Z}
+CISLO ::= 0..9{0..9}
+DECIMALNICISLO ::= 0..9{0..9}[.0..9{0..9}]
+BINARNICISLO ::= b%0|1{0|1}
+HEXADECIMALNICISLO ::= h%0..9|A..F{0..9|A..F}
+OPERATOR ::= +|-|/|*|^|&|!|"|"|and|or|not|==|!=
+RIDICIZNAKY ::= "("|")"|"{"|"}"
+KEYWORD ::= if|else|elif|while|for|break|continue|return
+"""
