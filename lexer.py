771650d389200f32cdbd4dec0ebdfb1d010f3d7e
Mikuláš Poul <mikulaspoul@gmail.com>
2013-10-19 21:09:43 +0200

    přidání operací a keywords do lexeru

--- lexer.py
+++ lexer.py
@@ -24,19 +24,55 @@ class Lexer:
 	NUMBER = "number" # token pro cislo, jeho hodnotou bude hodnota cisla
 	IDENT = "ident" # token pro identifikator, jeho hodnotou bude retezec s identifikatorem
 	# tohle jsou jen ukazky operatoru a klicovych slov, vase budou vypadat podle toho, jak bude vypadat vas jazyk
+
+	#keywords
 	KW_IF = "if" # klicove slovo if 
 	KW_ELSE = "else" # klicove slovo else
+	KW_ELSEIF = "elseif"
+	KW_WHILE = "while"
+	KW_FOR = "for"
+	KW_BREAK = "break"
+	KW_CONTINUE = "continue"
+	KW_RETURN = "return"
+	KW_FUNCTION = "function"
+
+	#operace
 	OP_ADD = "+" # operator scitani
 	OP_EQ = "==" # operator porovnani
 	OP_ASSIGN = "=" # operator prirazeni
+	OP_SUBSTRACT = "-" # operátor mínus
+	OP_MULTIPLY = "*" # operátor násobení
+	OP_DIVIDE = "/" # operátor dělení
+	OP_MOCNIT = "^" # operátor mocnění, nevím jak se to řekne česky, může to být: ^ a **
+	OP_FLOORDIVISION = "//" # operátor dělení, který ale bude floorovaný (9//2=4)
+	OP_REMAINDER = "%" # operátor zbytku po dělení
+	OP_AND = "AND" # a to: and AND &
+	OP_OR = "OR" # a to: or OR 
+	OP_NOT = "NOT" # a to: not NOT
+	OP_NOTEQUAL = "<>"
+	OP_BIGGER = ">"
+	OP_SMALLER = "<"
+	OP_BIGGEROREQUAL = ">="
+	OP_SMALLERORERUAL = "<="
+
 
 
 	def __init__(self):
 		""" Inicializuje lexikalni analyzator. Zalozi pole tokenu, zalozi seznam klicovych slov, atd. """
 		self._tokens = [] # seznam tokenu, ktere jsme uz naparsovali
 		self._keywords = {} # seznam klicovych slov. Ulozena jsou jako [klicove slovo] = typ tokenu odpovidajici
-		self._keywords["if"] = Lexer.KW_IF # takhle pridate vlastni klicova slova
+
+		#klíčová slova
+		self._keywords["if"] = Lexer.KW_IF 
 		self._keywords["else"] = Lexer.KW_ELSE
+		self._keywords["elseif"] = Lexer.KW_ELSEIF
+		self._keywords["while"] = Lexer.KW_WHILE
+		self._keywords["for"] = Lexer.KW_FOR
+		self._keywords["break"] = Lexer.KW_BREAK
+		self._keywords["continue"] = Lexer.KW_CONTINUE
+		self._keywords["return"] = Lexer.KW_RETURN
+		self._keywords["function"] = Lexer.KW_FUNCTION
+
 		self._top = 0 # ukazatel na token ktery vrati funkce topToken() vysvetlime si pozdeji
 		self._string = "" # aktualne zpracovavany retezec
 		self._pos = 0 # pozice v aktualne zpracovavanem retezci
