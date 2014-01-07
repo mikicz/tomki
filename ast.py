1bb4e999623a029ca26cb9ed7d81775abd00050c
Tomáš Bubeníček <tombuben@gmail.com>
2014-01-07 11:03:15 +0100

    Frame přesunut z frame.py do ast.py

--- ast.py
+++ ast.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+class Frame:
+
+	def __init__(self,parent):
+		self.locals =  {}
+		self.parent
+
+	def set (self, name, value):
+		self.locals[name] = value
+
+	def get (self, name):
+		if (name in self.locals):
+			return self.locals[name]
+		if (self.parent == None):
+			pass
+			#vyhoď error
+		return self.parent.get(self.name)
+
+
 class Block:
 	""" Blok prikazu. 
 
