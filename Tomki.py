951ce2677b514033c07364b091ff9d23a997e317
Tomáš Bubeníček <tombuben@gmail.com>
2014-01-23 13:33:21 +0100

    přidán shebang na spouštění programu v shellu

--- Tomki.py
+++ Tomki.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
 from lexer import Lexer
@@ -6,18 +7,20 @@ from slparser import Parser
 from ast import *
 
 def Tomki():
-	filename = sys.argv[1]
-	file = open(filename, "r")
-	string = file.read()
-	file.close()
+	if len(sys.argv)==2:
+		filename = sys.argv[1]
+		file = open(filename, "r")
+		string = file.read()
+		file.close()
+		Lex = Lexer()
+		Lex.analyzeString(string)
 
-	Lex = Lexer()
-	Lex.analyzeString(string)
+		Par = Parser(Lex)
+		AST = Par.parse()
 
-	Par = Parser(Lex)
-	AST = Par.parse()
-
-	frame = Frame(None)
-	AST.run(frame)
+		frame = Frame(None)
+		AST.run(frame)
+	else:
+		print("Error:\nNo file to interpret")
 
 Tomki()
